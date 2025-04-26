ce14e0bcdd3a888c086cc3ac963d44d047af0b38
Nguyen Tan Nam <nam.nguyentan.cit22@eiu.edu.vn>
2025-04-26 10:21:40 +0700

    Update Property.java

--- lab2/src/Property.java
+++ lab2/src/Property.java
@@ -2,4 +2,10 @@ public class Property {
     private String propertyID;
     private String description;
     private String propertyOwnerId;
+    public String getPropertyID() {
+        return propertyID;
+    }
+
+
+    
 }
