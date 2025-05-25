08adcf9f473a280065dd80f16c957693b48ddf43
Nguyen Tan Nam <nam.nguyentan.cit22@eiu.edu.vn>
2025-05-25 21:49:24 +0700

    update

--- Nam_Lab5/Lab5/src/q1/Client.java
+++ Nam_Lab5/Lab5/src/q1/Client.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Client {
 
-    public void requestCreateRentalContract(Contract builder) {
+    public Contract requestCreateRentalContract(Contract builder) {
         builder.BuildContactID("1").BuildPropertyID("2").BuildTenantID("3").BuildRentAmount(123);
         Contract contract = builder.signContract();
-        System.out.println(contract);
+        return contract;
     }
 
 }
